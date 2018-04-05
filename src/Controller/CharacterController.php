e02d64229fdb729f7b5e9cc3cbc65cf7b03c92a0
kevin loiseau <kevinpellin521@gmail.com>
2018-04-05 21:03:30 +0200

    modifications mineurs

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -10,7 +10,7 @@ class CharacterController extends AbstractController
         $characterManager = new CharacterManager();
         $fighters = $characterManager->selectAll();
         
-
         return $this->twig->render('Characters/index.html.twig', ['fighters' => $fighters]);
     }
+
 }
